8896b2ec4eaa98432dcfc4c77fb3b3d804327375
javad-hub <fasanehsepasian@gmail.com>
2021-10-19 17:31:22 +0330

    worked with models a bit

--- game/models.py
+++ game/models.py
@@ -1,3 +1,21 @@
 from django.db import models
 
 # Create your models here.
+
+class Category(models.Model):
+	name = models.CharField(max_length=200)
+
+
+	def __str__(self):
+		return self.name
+
+
+class Game(models.Model):
+	name = models.CharField(max_length=200)
+	category = models.ForeignKey(Category, on_delete= models.CASCADE,)
+	company = models.CharField(max_length=200)
+	date = models.DateTimeField(auto_now_add=True)
+
+
+	def __str__(self):
+		return f"{self.name} | {self.category}"
--- game/views.py
+++ game/views.py
@@ -1,6 +1,20 @@
 from django.shortcuts import render
 from django.views.generic import TemplateView
+from .models import Category, Game
 # Create your views here.
 
-class HomePageView(TemplateView):
-	template_name = 'home.html'
+choices = Category.objects.all().values_list('name','name')
+
+choice_list = []
+
+for item in choices:
+	choice_list.append(item)
+
+#class HomePageView(TemplateView):
+#	model = Category, Game
+#	template_name = 'index.html'
+
+def HomePageView(request):
+	return render(request, "index.html", {
+		'choice_list':choice_list,
+		})
